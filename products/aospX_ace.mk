0e3bba0b4f412697c3196c11601ad3b7de334736
Rob Beane <robbeane@gmail.com>
2012-01-06 01:53:42 -0500

    initial changes for aospX

---
+++ products/aospX_ace.mk
@@ -0,0 +1,29 @@
+# Inherit AOSP device configuration for ace
+$(call inherit-product, device/htc/ace/cm.mk)
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/aospX/products/common.mk)
+
+# Google Apps
+$(call inherit-product-if-exists, vendor/google/google-vendor.mk)
+
+#
+# Setup device specific product configuration.
+#
+PRODUCT_NAME := aospX_ace
+PRODUCT_BRAND := htc_wwe
+PRODUCT_DEVICE := ace
+PRODUCT_MODEL := Desire HD
+PRODUCT_MANUFACTURER := HTC
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_ace BUILD_FINGERPRINT=htc_wwe/htc_ace/ace:2.3.3/GRI40/87995:user/release-keys PRIVATE_BUILD_DESC="2.50.405.2 CL87995 release-keys"
+
+# aospX Version
+PRODUCT_VERSION_MAJOR = 1
+PRODUCT_VERSION_MINOR = 0
+PRODUCT_VERSION_MAINTENANCE = 0-AR3
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IML74K
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.modversion=aospX-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR).$(PRODUCT_VERSION_MAINTENANCE)-DesireHD
+
---
+++ products/common.mk
@@ -0,0 +1,73 @@
+# Generic aospX product
+PRODUCT_NAME := aospX
+PRODUCT_BRAND := aospX
+PRODUCT_DEVICE := generic
+
+# Include GSM stuff
+$(call inherit-product, vendor/aospX/products/gsm.mk)
+
+# Used by BusyBox
+KERNEL_MODULES_DIR:=/system/lib/modules
+
+# Tiny toolbox
+TINY_TOOLBOX:=true
+
+# Bring in all audio files
+include frameworks/base/data/sounds/AllAudio.mk
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.google.clientidbase=android-google \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.com.android.dateformat=MM-dd-yyyy \
+    ro.com.android.dataroaming=false
+
+# Required Common Packages
+PRODUCT_PACKAGES += \
+    Mms \
+    Camera \
+    FileManager \
+    Superuser \
+    Superuser.apk \
+    su
+
+# Live Wallpapers
+PRODUCT_PACKAGES += \
+    Basic \
+    PhaseBeam \
+    Galaxy4 \
+    HoloSpiralWallpaper \
+    NoiseField \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    VisualizationWallpapers \
+    librs_jni
+
+# Backuptool
+PRODUCT_COPY_FILES += \
+    vendor/aospX/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh
+
+# init.d
+PRODUCT_COPY_FILES += \
+    vendor/aospX/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
+    vendor/aospX/prebuilt/common/etc/init.d/01sysctl:system/etc/init.d/01sysctl \
+    vendor/aospX/prebuilt/common/etc/init.d/99complete:system/etc/init.d/99complete \
+    vendor/aospX/prebuilt/common/bin/sysinit:system/bin/sysinit
+
+PRODUCT_COPY_FILES += \
+    vendor/aospX/prebuilt/common/etc/resolv.conf:system/etc/resolv.conf \
+    vendor/aospX/prebuilt/common/etc/sysctl.conf:system/etc/sysctl.conf \
+    vendor/aospX/prebuilt/common/bin/fix_permissions:system/bin/fix_permissions
+
+# Enable SIP+VoIP on all targets
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
+# Boot Animation
+PRODUCT_COPY_FILES += \
+    vendor/aospX/prebuilt/common/bootanimation.zip:system/media/bootanimation.zip
+
+PRODUCT_PACKAGE_OVERLAYS += vendor/aospX/overlay/common
+PRODUCT_PACKAGE_OVERLAYS += vendor/aospX/overlay/dictionaries
